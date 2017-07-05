b162eb1913293084ed93c1239c23ce16b4e5e50c
Rory Goodman <goodmanr@tcd.ie>
2017-07-05 21:20:21 +0100

    first full draft

--- brushandpastey.js
+++ brushandpastey.js
@@ -7,6 +7,9 @@ function slide(){
 	var screen1=document.getElementById("screen1");
 	var screen2=document.getElementById("screen2");
 	var screen3=document.getElementById("screen3");
+	var sltext1=document.getElementById("bslide1");
+	var sltext2=document.getElementById("bslide2");
+	var sltext3=document.getElementById("bslide3");
 setInterval(function(){
 	switch(i){
 		case 1:
@@ -18,6 +21,10 @@ setInterval(function(){
 			screen1.style.transform="translate(-100%,0)";
 			screen2.style.transform="translate(-100%,0)";
 			screen3.style.transform="translate(-100%,0)";
+			sltext1.style.transform="translate(-100%,0)";
+			sltext2.style.transform="translate(-100%,0)";
+			sltext3.style.transform="translate(-100%,0)";
+			
 			break;
 
 		case 2:
@@ -29,6 +36,9 @@ setInterval(function(){
 				screen1.style.transform="translate(0,0)";
 				screen2.style.transform="translate(0,0)";
 				screen3.style.transform="translate(0,0)";
+				sltext1.style.transform="translate(0,0)";
+				sltext2.style.transform="translate(0,0)";
+				sltext3.style.transform="translate(0,0)";
 			}
 			else{
 				i=3;
@@ -38,6 +48,9 @@ setInterval(function(){
 				screen1.style.transform="translate(-200%,0)";
 				screen2.style.transform="translate(-200%,0)";
 				screen3.style.transform="translate(-200%,0)";
+				sltext1.style.transform="translate(-200%,0)";
+				sltext2.style.transform="translate(-200%,0)";
+				sltext3.style.transform="translate(-200%,0)";
 			}
 			break;
 		case 3:
@@ -49,6 +62,9 @@ setInterval(function(){
 			screen1.style.transform="translate(-100%,0)";
 			screen2.style.transform="translate(-100%,0)";
 			screen3.style.transform="translate(-100%,0)";
+			sltext1.style.transform="translate(-100%,0)";
+			sltext2.style.transform="translate(-100%,0)";
+			sltext3.style.transform="translate(-100%,0)";
 			break;
 		default:
 			i=1;
