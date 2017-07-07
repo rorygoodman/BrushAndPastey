729f7620844668dae3ad2690b1f3b8489a0aca79
Rory Goodman <goodmanr@tcd.ie>
2017-07-07 03:15:20 +0100

    sliders stop on click

--- brushandpastey.js
+++ brushandpastey.js
@@ -1,5 +1,9 @@
-var i = 1;
-var backwards=false;
+var itop = 1;
+var ibottom=1;
+var topauto=true;
+var bottomauto=true;
+var topbackwards=false;
+var bottombackwards=false;
 function slide(){
 	var slide1=document.getElementById("slider1");
 	var slide2=document.getElementById("slider2");
@@ -11,66 +15,101 @@ function slide(){
 	var sltext2=document.getElementById("bslide2");
 	var sltext3=document.getElementById("bslide3");
 setInterval(function(){
-	switch(i){
-		case 1:
-			backwards=false;
-			i=2;
-			slide1.style.transform="translate(-100%,0)";
-			slide2.style.transform="translate(-100%,0)";
-			slide3.style.transform="translate(-100%,0)";
-			screen1.style.transform="translate(-100%,0)";
-			screen2.style.transform="translate(-100%,0)";
-			screen3.style.transform="translate(-100%,0)";
-			sltext1.style.transform="translate(-100%,0)";
-			sltext2.style.transform="translate(-100%,0)";
-			sltext3.style.transform="translate(-100%,0)";
-			
-			break;
+	if(bottomauto){
+		switch(ibottom){
+			case 1:
+				bottombackwards=false;
+				ibottom=2;
+				slide1.style.transform="translate(-100%,0)";
+				slide2.style.transform="translate(-100%,0)";
+				slide3.style.transform="translate(-100%,0)";
+				
+				break;
 
-		case 2:
-			if(backwards){
-				i=1;
-				slide1.style.transform="translate(0,0)";
-				slide2.style.transform="translate(0,0)";
-				slide3.style.transform="translate(0,0)";
-				screen1.style.transform="translate(0,0)";
-				screen2.style.transform="translate(0,0)";
-				screen3.style.transform="translate(0,0)";
-				sltext1.style.transform="translate(0,0)";
-				sltext2.style.transform="translate(0,0)";
-				sltext3.style.transform="translate(0,0)";
-			}
-			else{
-				i=3;
-				slide1.style.transform="translate(-200%,0)";
-				slide2.style.transform="translate(-200%,0)";
-				slide3.style.transform="translate(-200%,0)";
-				screen1.style.transform="translate(-200%,0)";
-				screen2.style.transform="translate(-200%,0)";
-				screen3.style.transform="translate(-200%,0)";
-				sltext1.style.transform="translate(-200%,0)";
-				sltext2.style.transform="translate(-200%,0)";
-				sltext3.style.transform="translate(-200%,0)";
-			}
-			break;
-		case 3:
-			backwards=true;
-			i=2;
-			slide1.style.transform="translate(-100%,0)";
-			slide2.style.transform="translate(-100%,0)";
-			slide3.style.transform="translate(-100%,0)";
-			screen1.style.transform="translate(-100%,0)";
-			screen2.style.transform="translate(-100%,0)";
-			screen3.style.transform="translate(-100%,0)";
-			sltext1.style.transform="translate(-100%,0)";
-			sltext2.style.transform="translate(-100%,0)";
-			sltext3.style.transform="translate(-100%,0)";
-			break;
-		default:
-			i=1;
+			case 2:
+				if(bottombackwards){
+					ibottom=1;
+					slide1.style.transform="translate(0,0)";
+					slide2.style.transform="translate(0,0)";
+					slide3.style.transform="translate(0,0)";
+				}
+				else{
+					ibottom=3;
+					slide1.style.transform="translate(-200%,0)";
+					slide2.style.transform="translate(-200%,0)";
+					slide3.style.transform="translate(-200%,0)";
+				}
+				break;
+			case 3:
+				bottombackwards=true;
+				ibottom=2;
+				slide1.style.transform="translate(-100%,0)";
+				slide2.style.transform="translate(-100%,0)";
+				slide3.style.transform="translate(-100%,0)";
+				break;
+			default:
+				ibottom=1;
+		}
+			
+	}
+	if(topauto){
+		switch(itop){
+			case 1:
+				topbackwards=false;
+				itop=2;
+				screen1.style.transform="translate(-100%,0)";
+				screen2.style.transform="translate(-100%,0)";
+				screen3.style.transform="translate(-100%,0)";
+				sltext1.style.transform="translate(-100%,0)";
+				sltext2.style.transform="translate(-100%,0)";
+				sltext3.style.transform="translate(-100%,0)";
+				
+				break;
+
+			case 2:
+				if(topbackwards){
+					itop=1;
+					screen1.style.transform="translate(0,0)";
+					screen2.style.transform="translate(0,0)";
+					screen3.style.transform="translate(0,0)";
+					sltext1.style.transform="translate(0,0)";
+					sltext2.style.transform="translate(0,0)";
+					sltext3.style.transform="translate(0,0)";
+				}
+				else{
+					itop=3;
+					screen1.style.transform="translate(-200%,0)";
+					screen2.style.transform="translate(-200%,0)";
+					screen3.style.transform="translate(-200%,0)";
+					sltext1.style.transform="translate(-200%,0)";
+					sltext2.style.transform="translate(-200%,0)";
+					sltext3.style.transform="translate(-200%,0)";
+				}
+				break;
+			case 3:
+				topbackwards=true;
+				itop=2;
+				screen1.style.transform="translate(-100%,0)";
+				screen2.style.transform="translate(-100%,0)";
+				screen3.style.transform="translate(-100%,0)";
+				sltext1.style.transform="translate(-100%,0)";
+				sltext2.style.transform="translate(-100%,0)";
+				sltext3.style.transform="translate(-100%,0)";
+				break;
+			default:
+				itop=1;
+		}
 			
 	}
 },5000)
 	
 	return;
+}
+function noauto(slide){
+	if(slide==1){
+		topauto=false;
+	}
+	else{
+		bottomauto=false;
+	}
 }
